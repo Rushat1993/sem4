456599e2d5c6029c81784e475203770598c2614f
Rishat <risatnurullin53@gmail.com>
2022-08-07 14:55:45 +0300

    бmn

---
+++ tas4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+ 
+while (numberN >=10){
+     sum=sum+numberN%10;
+     
+      numberN = numberN/10;
+}
+  sum = sum + numberN;
+Console.WriteLine("Сумма цифр с учетом: " + sum);
